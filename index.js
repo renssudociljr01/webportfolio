34c6897bb2439cf62a02838546539efe80b0dea5
Renssu Docil Jr <136797044+renssudociljr01@users.noreply.github.com>
2023-12-06 18:28:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 {
     "capstone": {
-        "liveLink": "https://renssudociljr01.github.io/webpotfolio/",
+        "liveLink": "https://renssudociljr01.github.io/webportfolio/",
         "name": "Renato Renssu Docil Jr.",
         "isMulti": false
     }
